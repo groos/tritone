82607490fb057a8189c5107b72961f77367c6192
groos <ntgroos@gmail.com>
2019-05-11 12:55:55 -0500

    added todos

--- tritone.js
+++ tritone.js
@@ -7,8 +7,11 @@ var {chromatic, major} = require('./scales.js');
 module.exports = {
     scaleNotes: (key, scale) => {
         var scaleIndex = _.findIndex(chromatic.notes, (e) => e === key.toLowerCase());
+
+        // TODO - when to use # vs b ?
         var scaleResult = [chromatic.notes[scaleIndex]];
 
+        // TODO - select interval set via scale parameter
         scaleResult = scaleResult.concat(major.intervals.map((interval) => {
             scaleIndex += interval;
             return chromatic.notes[scaleIndex % chromatic.notes.length];
