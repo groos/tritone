01854a57c3ffced9394f4f1e07b582573b712be3
Nick Groos <ntgroos@gmail.com>
2019-06-05 15:01:53 -0500

    added tests for diatonic modes

--- test/diatonic.test.js
+++ test/diatonic.test.js
@@ -22,6 +22,29 @@ test('array with notes in g mixolydian scale. requires modal shit', () => {
     expect(tritone.scaleNotes('g', 'mixolydian')).toStrictEqual(["g","a","b","c","d","e","f"]);
 });
 
+test('array of diatonic chords in the c major scale', () => {
+    var chords = tritone.diatonicChords('c', 'major');
+    //Major 7
+    expect(chords[0].root).toStrictEqual('C');
+    expect(chords[0].chordType).toStrictEqual('M7');
+    expect(chords[0].notes).toStrictEqual(["c","e","g","b"]);
+
+    //Minor 7
+    expect(chords[2].root).toStrictEqual('E');
+    expect(chords[2].chordType).toStrictEqual('m7');
+    expect(chords[2].notes).toStrictEqual(["e","g","b","d"]);
+
+    //Dom 7
+    expect(chords[4].root).toStrictEqual('G');
+    expect(chords[4].chordType).toStrictEqual('7');
+    expect(chords[4].notes).toStrictEqual(["g","b","d","f"]);
+
+    // m7b5
+    expect(chords[6].root).toStrictEqual('B');
+    expect(chords[6].chordType).toStrictEqual('m7b5');
+    expect(chords[6].notes).toStrictEqual(["b","d","f","a"]);
+})
+
 /*
 f major: f,g,a,bb,c,d,e
 f ionian: f,g,a,bb,c,d,e
