d7c7cf60c5bda80f9bba05899986f491404fd27b
groos <ntgroos@gmail.com>
2019-06-05 18:11:50 -0500

    updated readme

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 var Tritone = require('./src/tritone.js');
 
 var tritone = new Tritone();
+
+var prettyPrint = (label, anArray) => {
+    console.log('------------------------------');
+    console.log(label);
+    anArray.forEach((element) => {
+        console.log(JSON.stringify(element));
+    })
+}
+
+prettyPrint('B Major Notes', tritone.scaleNotes('b', 'ionian'));
+prettyPrint('B Major Chords', tritone.diatonicChords('b', 'major'));
+prettyPrint('G Mixolydian Chords:', tritone.diatonicChords('g', 'mixolydian'));
+
+
+
 console.log('c major chords: ' + JSON.stringify(tritone.diatonicChords('c', 'major')));
-console.log('b major chords: ' + JSON.stringify(tritone.diatonicChords('b', 'major')));
+console.log('g mixolydian notes: ' + JSON.stringify(tritone.scaleNotes('g', 'mixolydian')));
 console.log('g mixolydian chords: ' + JSON.stringify(tritone.diatonicChords('g', 'mixolydian')));
 console.log('g mixolydian: ' + JSON.stringify(tritone.scaleNotes('g', 'mixolydian')));
